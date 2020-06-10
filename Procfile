59cf26e0d065107095f6d6a544c6ba8ffed0bcba
Jesselle G <dqp6065@autuni.ac.nz>
2020-06-10 15:52:02 +1200

    made main_pageJS.js as main code

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: npm start
+web: node main_pageJS.js
--- main_pageJS.js
+++ main_pageJS.js
@@ -1,23 +1,30 @@
 var searchFieldVal="";
 
-const {Client} = require('pg');
-const connectionString = 'postgres://wiucfmzirbegqr:9624d9c375c7f9c8bc973d6396545a3a3d789e6e73a73df5e5bbb1ed519fe234@ec2-23-20-129-146.compute-1.amazonaws.com:5432/dbh1gu7tl09u8';
+const express = require('express');
+const app = new express();
 
-const client = new Client({
-    connectionString: connectionString,
-    ssl: {
-        rejectUnauthorized: false
-    }
-});
-
-    client.connect();
-    //client.query('SELECT DISTINCT * FROM source WHERE title LIKE \'%Empirical%\'', (err, res) => {
-    client.query('SELECT s.title, array_to_string(array_agg(a.last_name), \',\') AS "Authors" FROM source s INNER JOIN authorsrc ass ON s.id = ass.source_id INNER JOIN author a ON a.id = ass.author_id WHERE s.title LIKE \'%Empirical%\' GROUP BY s.title', 
-     (err, res) => {
-         
-    console.log(err, res);
-    client.end();
+app.get('/',function(request, response){
+    response.sendFile('./main_page.html');
 })
+
+// const {Client} = require('pg');
+// const connectionString = 'postgres://wiucfmzirbegqr:9624d9c375c7f9c8bc973d6396545a3a3d789e6e73a73df5e5bbb1ed519fe234@ec2-23-20-129-146.compute-1.amazonaws.com:5432/dbh1gu7tl09u8';
+
+// const client = new Client({
+//     connectionString: connectionString,
+//     ssl: {
+//         rejectUnauthorized: false
+//     }
+// });
+
+//     client.connect();
+//     //client.query('SELECT DISTINCT * FROM source WHERE title LIKE \'%Empirical%\'', (err, res) => {
+//     client.query('SELECT s.title, array_to_string(array_agg(a.last_name), \',\') AS "Authors" FROM source s INNER JOIN authorsrc ass ON s.id = ass.source_id INNER JOIN author a ON a.id = ass.author_id WHERE s.title LIKE \'%Empirical%\' GROUP BY s.title', 
+//      (err, res) => {
+         
+//     console.log(err, res);
+//     client.end();
+// })
 
 function setValueField(){
     var e = document.getElementById('searchFields').value;
